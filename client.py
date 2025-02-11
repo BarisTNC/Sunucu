433f6410d9579965d315f3f91ab0dfde4b62c652
BarisTNC <Sakarya175@gmail.com>
2025-02-11 23:29:35 +0300

    make it better

--- client.py
+++ client.py
@@ -49,5 +49,5 @@ class RemoteClient:
                 print("\n[INFO] Bağlantı kapatıldı.")
 
 if __name__ == '__main__':
-    client = RemoteClient('http://127.0.0.1:5000')
+    client = RemoteClient('http://klinkzapp/app/')
     client.start()
